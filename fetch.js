028f2aa1bd938567039242af6f79cb182569406c
Fabian Leon <fabianleon1209@gmail.com>
2024-04-14 13:38:00 -0600

    Se avanza con cambios en interfaz y js

--- fetch.js
+++ fetch.js
@@ -1,12 +1,17 @@
-console.log("Hallooo")
+console.log("Hallooo");
 
-
+var div_usuarios=document.querySelector('#usuarios')
 
 fetch("https://reqres.in/api/users")
-.then(data=>data.json())
-.then(users=>{
+  .then((data) => data.json())
+  .then((users) => {
+    usuarios = users.data;
+    console.log(usuarios);
 
-    usuarios=users.data;
-    console.log(usuarios)
-
-})
+    usuarios.map((user,i)=>{
+        let nombre=document.createElement('p')
+        nombre.innerHTML= i+1 + " "+ user.first_name + " "+ user.last_name;    
+        div_usuarios.appendChild(nombre)
+        document.querySelector('.loading').style.display='none';
+    })
+  });
