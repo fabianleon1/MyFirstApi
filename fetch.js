4abaf692037995d2b1ace36512ae51a4c8d2ddba
Fabian Leon <fabianleon1209@gmail.com>
2024-04-17 21:18:16 -0600

    Modificando todo

--- fetch.js
+++ fetch.js
@@ -1,17 +1,37 @@
 console.log("Hallooo");
 
 var div_usuarios=document.querySelector('#usuarios')
+var div_janet=document.querySelector('#janet')
+ //peticion al servidor
 
-fetch("https://reqres.in/api/users")
-  .then((data) => data.json())
+  getUsuarios().then((data) => data.json())
   .then((users) => {
-    usuarios = users.data;
-    console.log(usuarios);
+    listadoUsuarios ( users.data);
+    return getJanet();
 
-    usuarios.map((user,i)=>{
-        let nombre=document.createElement('p')
-        nombre.innerHTML= i+1 + " "+ user.first_name + " "+ user.last_name;    
-        div_usuarios.appendChild(nombre)
-        document.querySelector('.loading').style.display='none';
-    })
   });
+
+  function getUsuarios(){
+    return fetch("https://reqres.in/api/users")
+  }
+  function getJanet(){
+    return fetch("https://reqres.in/api/users?page=2")
+  }
+function listadoUsuarios(usuarios){
+
+  usuarios.map((user,i)=>{
+    let nombre=document.createElement('H2') //creando elemento que queramos
+    nombre.innerHTML= i+1 + " "+ user.first_name + " "+ user.last_name;   //concatenamos a la variable el texto que vamos a meter 
+    div_usuarios.appendChild(nombre) //añadimos al div cada usuario
+    document.querySelector('.loading').style.display='none';
+  })
+}
+function mostrarJanet(usuario){
+
+ 
+    let nombre=document.createElement('') //creando elemento que queramos
+    nombre.innerHTML= i+1 + " "+ user.first_name + " "+ user.last_name;   //concatenamos a la variable el texto que vamos a meter 
+    div_janet.appendChild(nombre) //añadimos al div cada usuario
+    document.querySelector('.loading').style.display='none';
+
+}
